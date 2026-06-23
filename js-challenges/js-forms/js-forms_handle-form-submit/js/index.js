df7aca95726be511510477e0a75fb63aa3530f7d
Sabrina <sabrinarach.sr@gmail.com>
2026-06-23 13:06:16 +0200

    handle form submit: made the code easier

--- js-challenges/js-forms/js-forms_handle-form-submit/js/index.js
+++ js-challenges/js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,20 +1,22 @@
 //console.clear();
 
 const form = document.querySelector('[data-js="form"]');
-const firstNameInput = document.querySelector('[name="firstName"]');
+//const firstNameInput = document.querySelector('[name="firstName"]');
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const formData = new FormData(form);
+ const FormElement = event.target;
+
+  const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
 
   console.log(data);
 
-  form.reset();
-  firstNameInput.focus();
+  event.target.reset();
+  event.target.firstName.focus();
 
-  const ageBadnessSum = Number(data.age) + Number(data.badness);
+  const ageBadnessSum = Number(data.age) + Number(data.badness); // mit Number werden die Strings aus data in Zahlen umgewandelt (da die Werte aus object strings sind)
   console.log(
     `The age-badness-sum of "${data.firstName}" is "${ageBadnessSum}"`,
   );
