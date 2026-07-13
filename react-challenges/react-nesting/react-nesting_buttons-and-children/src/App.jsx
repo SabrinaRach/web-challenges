d4fe24912c394f9a47886adb14c858cabb1a20fa
Sabrina <sabrinarach.sr@gmail.com>
2026-07-13 15:11:30 +0200

    task complete and added new file Button.jsx

---
+++ react-challenges/react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+import Button from "./Buttons.jsx";
+
+export default function App() {
+  return (
+    <main>
+      <Button>this is the first button</Button>
+      <Button>click me</Button>
+      <Button>submit</Button>
+      <Button>cancel</Button>
+    </main>
+  );
+}
