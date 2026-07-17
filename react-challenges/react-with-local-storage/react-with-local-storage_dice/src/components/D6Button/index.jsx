723d891f521395a67b8fd26a0354a3e1991ff83f
Sabrina <sabrinarach.sr@gmail.com>
2026-07-17 17:07:34 +0200

    task complete

---
+++ react-challenges/react-with-local-storage/react-with-local-storage_dice/src/components/D6Button/index.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { getD6Roll } from "../../utils";
+import D6 from "../D6";
+import "./D6Button.css";
+
+export function D6Button({value, onRoll}) {
+  /* const [rolls, setRolls] = useState([]); wird in App geliftet und Props in D6Button function setzen: rolls, setRolls */
+
+  /* const handleRoll = () => {
+    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+  }; 
+
+  const currentRollValue = rolls[0]?.value; */
+
+  return (
+    <button className="d6-button" type="button" onClick={onRoll}>
+      <D6 value={value} />
+    </button>
+  );
+}
